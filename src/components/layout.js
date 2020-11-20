8a589c8c62364a1355652c5718befedb9057965e
cilg017 <cilg017@gmail.com>
2020-11-20 14:29:09 -0500

    initial commit

--- src/components/layout.js
+++ src/components/layout.js
@@ -12,6 +12,8 @@ import { useStaticQuery, graphql } from "gatsby"
 import Header from "./header"
 import "./layout.css"
 
+import { ChakraProvider, theme as defaultTheme, extendTheme } from "@chakra-ui/core"
+
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
@@ -23,25 +25,58 @@ const Layout = ({ children }) => {
     }
   `)
 
+  const customTheme = extendTheme({
+    colors: {
+      brand: {
+        900: "#b4d455",
+        800: "#153e75",
+        700: "#2a69ac",
+      },
+      cyan: '#0BC5EA',
+      green: '#68D391',
+      purple: '#B794F4',
+      red: '#E53E3E',
+      white: '#FFF',
+      black: '#000',
+      grey: {
+        100: '#EDF2F7',
+        300: '#CBD5E0',
+        500: '#718096',
+        700: '#2D3748',
+        900: '#171923'
+      },
+      orange: '#ED8936',
+      blue: '#63B3ED',
+      pink: '#FBB6CE'
+    },
+    fonts: {
+      primary: 'Courier New, monospace',
+      handwriting: 'Bradley Hand, cursive'
+    }
+  })
+
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer style={{
-          marginTop: `2rem`
-        }}>
-          © {new Date().getFullYear()}, Built with
+      <ChakraProvider theme={customTheme}>
+
+        <Header siteTitle={`carly website`} />
+        <div
+          style={{
+            margin: `0 auto`,
+            maxWidth: 960,
+            padding: `0 1.0875rem 1.45rem`,
+          }}
+        >
+          <main>{children}</main>
+          <footer style={{
+            marginTop: `2rem`
+          }}>
+            © {new Date().getFullYear()}, Built with
           {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
-      </div>
+            <a href="https://www.gatsbyjs.com">Gatsby</a>
+          </footer>
+        </div>
+      </ChakraProvider>
     </>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,36 @@
 import React from "react"
 import { Link } from "gatsby"
+import { Box, Flex, Image, Text } from "@chakra-ui/core"
+import logo from '../images/cobb.jpg';
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
+    <SEO title="carlys website" />
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
     <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    <Box bg='white' height="250px" width="500px" border="2px solid" borderColor="grey" borderRadius='3px' p={1}>
+      <Flex flexDirection='row' id='#top-row' bg='green' h='25%'>
+        <Flex as={Text} pr='30px' bg='orange' fontFamily='primary'>MASSACHUSETTS</Flex>
+        <Flex as={Text} bg='purple'>DRIVER'S LICENSE</Flex>
+      </Flex>
+      <Flex flexDirection='row' bg='cyan' h='75%'>
+        <Flex id="#left-side" flexDirection='column' bg='grey.100' w='33%'>
+          <Box bg='red' h='75%' p={2}>
+            <Image src={logo} w='100%' h='100%' borderRadius='3px' />
+          </Box>
+          <Box as={Text} bg='purple' h='25%' fontFamily='handwriting'>Cobbler Bobbler Ilg</Box>
+        </Flex>
+        <Flex id='#right-side' flexDirection='column'>
+          <Box id='#section-with-issue-dates' bg='blue' >issue dates</Box>
+          <Box id='#address-name-section' bg='pink'>name and address and what not</Box>
+          <Box id='#eyes-sex-height-donor-section' bg='grey.300'><Text>hello</Text></Box>
+        </Flex>
+      </Flex>
+    </Box>
   </Layout>
 )
 
